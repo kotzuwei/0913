6f62f2daed6d39e156554e802f063a5c62a3482b
kotzuwei <owo6289@gmail.com>
2024-11-15 11:53:01 +0800

    1115

---
+++ server.js
@@ -0,0 +1,26 @@
+//install: node js
+//install web server package: express >npm install express
+var express = require("express");
+var server = express();
+
+
+//web root
+server.use(express.static(__dirname+"/AgencyProject"));
+
+
+//web get
+server.get("/:DDDDD", (req, res)=>{
+    res.send("早ㄤ");
+})
+server.get("/services", (req, res)=>{
+    var services = [
+        {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
+        {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
+    ];
+    res.send(services);
+})
+
+
+server.listen(80, ()=>{
+    console.log(":DDDDD");
+})
