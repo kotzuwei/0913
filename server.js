2831279bb0affd8095c0e06a9ec889ef29b06b72
kotzuwei <owo6289@gmail.com>
2024-11-22 12:02:28 +0800

    1122

--- server.js
+++ server.js
@@ -2,16 +2,27 @@
 //install web server package: express >npm install express
 var express = require("express");
 var server = express();
+var bodyParser = require("body-parser");
 
 
 //web root
 server.use(express.static(__dirname+"/AgencyProject"));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded());
 
 
 //DB
 var DB = require("nedb-promises");
-var Profolio = DB.create(__dirname+"/profolio.db");
-Profolio.insert({modal: "#portfolioModal1", imgSrc:"roundicons.png", heading:"Round Icons", text:"Graphic Design"})
+var ProfolioDB = DB.create(__dirname+"/profolio.db");
+var ProfolioDB = DB.create(__dirname+"/contact_me.db");
+    // ProfolioDB.insert([ 
+    //     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+    //     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+    //     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" },
+    //     { href: "#portfolioModal1", imgSrc: "img/portfolio/roundicons.png", title: "Round Icons", text: "Graphic Design" },
+    //     { href: "#portfolioModal2", imgSrc: "img/portfolio/startup-framework.png", title: "Startup Framework", text: "Website Design" },
+    //     { href: "#portfolioModal3", imgSrc: "img/portfolio/treehouse.png", title: "Treehouse", text: "Website Design" }
+    // ])
 
 
 
@@ -25,6 +36,28 @@ server.get("/services", (req, res)=>{
         {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
     ];
     res.send(services);
+})
+
+
+server.get("/profolio", (req,res)=>{
+    //db
+    ProfolioDB.find({}).then(results=>{
+        if(results != null)
+        {
+            res.send(results);
+        }
+        else
+        {
+            res.send("error");
+        }
+    })
+
+})
+
+
+server.post("contact_me", (req,res)=>{
+    ContactDB.insert(req.body);
+    res.send("ok");
 })
 
 
